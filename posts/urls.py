c5c6354b8c8bee21edb200a04be3925f3c96c245
Jose Torres <jose.a.torres513@gmail.com>
2025-02-24 22:25:09 -0500

    FSDI 113 Assignment 3

--- posts/urls.py
+++ posts/urls.py
@@ -3,10 +3,10 @@ from posts import views
 
 
 urlpatterns = [
-    path("new/", views.PostCreateView.as_view(), name="create"),
     path("<int:pk>/edit/", views.PostUpdateView.as_view(), name="edit"),
     path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
     path("<int:pk>/detail/", views.PostDetailView.as_view(), name="detail"),
+    path("new/", views.PostCreateView.as_view(), name="new"),
     path("list/", views.PostListView.as_view(), name="list"),
     path("drafts/", views.DraftPostListView.as_view(), name="drafts"),
     path("archive/", views.ArchivedPostListView.as_view(), name="archive"),
